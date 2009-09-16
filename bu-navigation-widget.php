509c2736cf4482931d06ac554b53d9482e76af5d
(no author) <(no author)@00b09ed4-20f5-4458-a511-c3642c8cf453>
2009-09-16 17:36:51 +0000

    Adding base BU navigation plugin to two-eight branch

---
+++ bu-navigation-widget.php
@@ -0,0 +1,81 @@
+<?php
+class BU_Widget_Pages extends WP_Widget 
+{
+	function WP_Widget_Pages() 
+	{
+		$widget_ops = array('classname' => 'widget_bu_pages', 'description' => __( "Navigation list of your site's pages" ) );
+		$this->WP_Widget('bu_pages', __('BU Pages'), $widget_ops);
+	}
+
+	function widget( $args, $instance ) 
+	{
+		extract( $args );
+
+		$title = apply_filters('widget_title', empty( $instance['title'] ) ? __( 'Pages' ) : $instance['title']);
+		$sortby = empty( $instance['sortby'] ) ? 'menu_order' : $instance['sortby'];
+		$exclude = empty( $instance['exclude'] ) ? '' : $instance['exclude'];
+
+		if ( $sortby == 'menu_order' )
+			$sortby = 'menu_order, post_title';
+
+		$out = bu_navigation_list_pages( apply_filters('widget_bu_pages_args', array('title_li' => '', 'echo' => 0, 'sort_column' => $sortby, 'exclude' => $exclude) ) );
+
+		if ( !empty( $out ) ) 
+		{
+			echo $before_widget;
+			if ( $title)
+				echo $before_title . $title . $after_title;
+		?>
+		<ul>
+			<?php echo $out; ?>
+		</ul>
+		<?php
+			echo $after_widget;
+		}
+	}
+
+	function update( $new_instance, $old_instance ) 
+	{
+		$instance = $old_instance;
+		
+		$instance['title'] = strip_tags($new_instance['title']);
+		
+		if ( in_array( $new_instance['sortby'], array( 'post_title', 'menu_order', 'ID' ) ) ) 
+		{
+			$instance['sortby'] = $new_instance['sortby'];
+		} 
+		else 
+		{
+			$instance['sortby'] = 'menu_order';
+		}
+
+		$instance['exclude'] = strip_tags( $new_instance['exclude'] );
+
+		return $instance;
+	}
+
+	function form( $instance ) 
+	{
+		//Defaults
+		$instance = wp_parse_args( (array) $instance, array( 'sortby' => 'post_title', 'title' => '', 'exclude' => '') );
+		$title = esc_attr( $instance['title'] );
+		$exclude = esc_attr( $instance['exclude'] );
+	?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+		<p>
+			<label for="<?php echo $this->get_field_id('sortby'); ?>"><?php _e( 'Sort by:' ); ?></label>
+			<select name="<?php echo $this->get_field_name('sortby'); ?>" id="<?php echo $this->get_field_id('sortby'); ?>" class="widefat">
+				<option value="post_title"<?php selected( $instance['sortby'], 'post_title' ); ?>><?php _e('Page title'); ?></option>
+				<option value="menu_order"<?php selected( $instance['sortby'], 'menu_order' ); ?>><?php _e('Page order'); ?></option>
+				<option value="ID"<?php selected( $instance['sortby'], 'ID' ); ?>><?php _e( 'Page ID' ); ?></option>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e( 'Exclude:' ); ?></label> <input type="text" value="<?php echo $exclude; ?>" name="<?php echo $this->get_field_name('exclude'); ?>" id="<?php echo $this->get_field_id('exclude'); ?>" class="widefat" />
+			<br />
+			<small><?php _e( 'Page IDs, separated by commas.' ); ?></small>
+		</p>
+	<?php
+	}
+}
+?>
---
+++ bu-navigation.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Plugin Name: Page Navigation 
+Version: 0.1
+Author URI: http://www.bu.edu/tech/help/
+Description: Provides alternative navigation elements designed for blogs with large page counts
+Author: Networked Information Services
+*/
+
+/* BU Navigation constants */
+define('BU_NAV_META_PAGE_LABEL', '_bu_cms_navigation_page_label'); // name of meta_key used to hold navigation labels
+define('BU_NAV_META_PAGE_EXCLUDE', '_bu_cms_navigation_exclude'); // name of meta_key used to exclude pages from navigation
+
+/* Widgets */
+require_once('bu-navigation-widget.php');
+
+/**
+* Returns an array of page objects indexed by page ID
+* TODO: Function incomplete; most arguments ignored. Sort order should allow +1 column
+* @param $args mixed Wordpress-style arguments (string or array)
+* @return array Array of pages keyed on page ID or FALSE on problem
+*/
+function bu_navigation_get_pages($args = '')
+{
+	global $wpdb;
+
+	$defaults = array(
+		'in_section' => NULL,
+		'child_of' => 0, 
+		'sort_order' => 'ASC',
+		'sort_column' => 'post_title', 
+		'hierarchical' => 1,
+		'exclude' => '', 
+		'include' => '',
+		'meta_key' => '', 
+		'meta_value' => '',
+		'primary' => '',
+		'authors' => '', 
+		'max_items' => '',
+		'post_status' => 'publish'
+		);
+
+	$r = wp_parse_args($args, $defaults); // parsed arguments
+
+	/* list of fields to SELECT */
+
+	$fields = array(
+		'ID',
+		'post_date',
+		'post_title',
+		'post_excerpt',
+		'post_name',
+		'post_parent',
+		'guid',
+		'menu_order',
+		'post_type'
+		);
+
+	$params = array(); /* will hold parameters to pass to query */
+
+	/* build the query */
+	$query = sprintf('SELECT %s FROM %s ', implode(',', $fields), $wpdb->posts);
+
+	/* only get pages and external links */
+	$query .= " WHERE post_type IN('page','link') ";
+
+	/* restrict status of pages */
+	$query .= ' AND post_status = %s ';
+	array_push($params, $r['post_status']);
+
+	/* result sorting */
+	$sort_order = (strtoupper($r['sort_order']) == 'DESC') ? 'DESC' : 'ASC'; // will default to ASC if invalid arg found
+	$sort_field = (in_array($r['sort_column'], $fields)) ? $r['sort_column'] : 'post_title'; // defaults to post_title if invalid arg found
+	$query .= sprintf(' ORDER BY %s %s ', $sort_field, $sort_order);
+
+	$sql = $wpdb->prepare($query, $params);
+
+	$pages = $wpdb->get_results($query, OBJECT_K); // get results as objects in an array keyed on posts.ID 
+
+	if ((!is_array($pages)) || (count($pages) == 0)) return FALSE;
+
+	return $pages;
+}
+
+function bu_navigation_list_pages($args = '')
+{
+	$defaults = array(
+		'in_section' => NULL,
+		'depth' => 0, 
+		'show_date' => '',
+		'date_format' => get_option('date_format'),
+		'child_of' => 0, 
+		'exclude' => '',
+		'title_li' => __('Pages'), 
+		'echo' => 1,
+		'authors' => '', 
+		'sort_column' => 'menu_order, post_title'
+		);
+
+	$r = wp_parse_args($args, $defaults);
+	
+	$pages = bu_navigation_get_pages($r);
+
+	$output = '';
+
+	$output .= walk_page_tree($pages, $r['depth'], $current_page, $r);
+
+	return $output;
+}
+
+function bu_navigation_widgets_init() 
+{
+	if ( !is_blog_installed() )
+		return;
+
+	register_widget('BU_Widget_Pages');
+
+	do_action('widgets_init');
+}
+
+add_action('init', 'bu_navigation_widgets_init', 1);
+?>

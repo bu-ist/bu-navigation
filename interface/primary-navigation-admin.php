341b503835e7b9404de54e67714de68808c4fac4
(no author) <(no author)@00b09ed4-20f5-4458-a511-c3642c8cf453>
2009-10-02 18:16:01 +0000

    Updated navigation code Loader loads nav plugin first

---
+++ interface/primary-navigation-admin.php
@@ -0,0 +1,46 @@
+<div class="wrap">
+	<h2><?php _e('Primary Navigation'); ?></h2>
+
+	<p>
+		Your primary navigation bar is the horizontal bar at the top of every 
+		page which shows users your top-level navigation no matter where they
+		are.
+	</p>
+
+	<form id="bu_navigation_primary_navigation" action="" method="post">
+		<input type="hidden" name="bu_navigation_primary_save" value="save" />
+		<p>
+			<input type="checkbox" id="bu_navigation_primarynav" name="bu_navigation_primarynav" value="1" <?php if ($bu_navigation_primarynav) echo 'checked="checked"'; ?> /> 
+			<strong><label for="bu_navigation_primarynav">Display primary navigation bar</label></strong>
+			<br />
+			Toggles the primary navigation bar on and off.
+		</p>
+
+		<p>
+			<input type="text" id="bu_navigation_primarynav_max" name="bu_navigation_primarynav_max" value="<?php echo $bu_navigation_primarynav_max; ?>" size="2" maxlength="2" />
+			<strong><label for="bu_navigation_primarynav_max">Maximum items</label></strong>
+			<br />
+			Maximum number of top-level items to display in the primary 
+			navigation bar.
+		</p>
+
+		<p>
+			<input type="checkbox" id="bu_navigation_primarynav_dive" name="bu_navigation_primarynav_dive" value="1" <?php if ($bu_navigation_primarynav_dive) echo 'checked="checked"'; ?> /> 
+			<strong><label for="bu_navigation_primarynav_dive">Use drop-down menus</label></strong>
+			<br />
+			If checked, any top-level pages with children will expand to display 
+			those children when somebody moves their mouse over the link.	
+		</p>
+		<p style="margin-left: 2em;">
+			<strong><label for="bu_navigation_primarynav_depth">Display</label></strong>
+			<input type="textbox" id="bu_navigation_primarynav_depth" name="bu_navigation_primarynav_depth" value="<?php echo $bu_navigation_primarynav_depth; ?>" size="2" maxlength="2" />
+			<strong><label for="bu_navigation_primarynav_depth">levels of children</label></strong>
+			<br />
+			Note that not all themes are able to display more than one level of children.
+		</p>
+
+		<p class="submit">
+			<input type="submit" name="Submit" value="Save Changes" />
+		</p>
+	</form>
+</div>

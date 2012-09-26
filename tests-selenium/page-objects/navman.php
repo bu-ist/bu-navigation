ccf884264dd72cdfacf53d526998d1ff71a275d2
mgburns <mgburns@00b09ed4-20f5-4458-a511-c3642c8cf453>
2012-09-26 19:14:10 +0000

    Added Selenium tests directory, test file for navman interface

---
+++ tests-selenium/page-objects/navman.php
@@ -0,0 +1,231 @@
+<?php
+
+/**
+ * Edit Order page (Edit Order + Post Type)
+ */ 
+class BUN_Navman_Page {
+
+	protected $webdriver = null;
+	protected $group_form = null;
+
+	protected $leaf_id_prefix;
+
+	/* URL's */
+	const NAVMAN_BASE_URL = '/wp-admin/edit.php?page=bu-navigation/bu-navman.php';
+	const TEST_XPATH = "//h2[contains(text(),'Edit Navigation')]";
+
+	/* Markup constants */
+
+	// Forms
+	const NAVMAN_FORM = 'navman_form';
+
+	const NAVMAN_EDIT_BTN = 'bu_navman_edit';
+	const NAVMAN_DELETE_BTN = 'bu_navman_delete';
+	const NAVMAN_SAVE_BTN = 'bu_navman_save';
+	const NAVMAN_EXPAND_BTN = 'navman_expand_all';
+	const NAVMAN_COLLAPSE_BTN = 'navman_collapse_all';
+
+	// Types
+	const TYPES_PAGE = 'page';
+	const TYPES_FOLDER = 'folder';
+	const TYPES_EXCLUDED = 'page_excluded';
+
+	/**
+	 * Load the Navman page
+	 */ 
+	function __construct( $webdriver, $post_type = 'page' ) {
+
+		$this->webdriver = $webdriver;
+
+		// Generate request URL
+		$request_url = self::NAVMAN_BASE_URL . '&post_type=' . $post_type;
+		$this->webdriver->open( $request_url  );
+
+		try {
+			$this->webdriver->findElementBy( LocatorStrategy::xpath, self::TEST_XPATH );
+		} catch( NoSuchElementException $e ) {
+			throw new Exception('BU Navigation Edit Order failed to load -- Unable to load URL: ' . $request_url );
+		}
+
+		$this->form = new SeleniumFormHelper( $this->webdriver, self::NAVMAN_FORM );
+		$this->leaf_id_prefix = 'p';
+	}
+
+	const GET_PAGE_XPATH = '//li[@id="%s"]';
+
+	/**
+	 * Return either the list item or clickable anchor for the given page ID
+	 * 
+	 * @param $id int Post ID to look for
+	 * @param $el string which markup component to look for -- root 'li', or child 'a'
+	 */ 
+	public function getPage( $id, $el = 'li' ) {
+
+		$id = $this->leaf_id_prefix . $id;
+
+		$xpath = sprintf(self::GET_PAGE_XPATH,$id);
+
+		if( 'a' == $el )
+			$xpath .= '/a';
+
+ 		return $this->webdriver->findElementBy( LocatorStrategy::xpath, $xpath );
+
+	}
+
+	/**
+	 * @todo option to expand all before running this
+	 */ 
+	public function getSections() {
+ 		
+ 		return $this->webdriver->findElementsBy( LocatorStrategy::cssSelector, '#navman_container li[rel="folder"]');
+
+	}
+
+	public function selectPage( $id ) {
+
+		$id = $this->leaf_id_prefix . $id;
+
+		$xpath = sprintf(self::GET_PAGE_XPATH . '/a' ,$id);
+
+ 		try {
+ 			$page = $this->webdriver->findElementBy( LocatorStrategy::xpath, $xpath );
+ 			$page->click();
+ 			return true;
+ 		} catch( NoSuchElementException $e ) {
+ 			return false;
+ 		}
+
+	}
+
+	public function editPage( $id ) {
+
+		$this->selectPage( $id );
+		$btn = $this->webdriver->findElementBy( LocatorStrategy::id, self::NAVMAN_EDIT_BTN );
+		$btn->click();
+		sleep(2);
+
+	}
+
+	public function deletePage( $id ) {
+
+		$this->selectPage( $id );
+		$btn = $this->webdriver->findElementBy( LocatorStrategy::id, self::NAVMAN_DELETE_BTN );
+		$btn->click();
+		sleep(1);
+
+	}
+
+	const OPEN_SECTION_ICON_XPATH = '//li[@id="%s"]/ins[@class="jstree-icon"]';
+
+	public function openSection( $id ) {
+		$id = $this->leaf_id_prefix . $id;
+
+		$xpath = sprintf( self::OPEN_SECTION_ICON_XPATH, $id );
+		$el = $this->webdriver->findElementBy( LocatorStrategy::xpath, $xpath );
+
+		$el->click();
+
+		sleep(1);
+
+	}
+
+	const LEAF_XPATH = '//li[@id="%s"]/a';
+	const MOVE_BEFORE_Y_OFFSET = 2;
+	const MOVE_AFTER_Y_OFFSET = 5;
+
+	/**
+	 * Move a page using the jstree drag and drop interface
+	 * 
+	 * @todo this isn't working correctly, perhaps due to issues with webdrivers moveto method
+	 */ 
+	public function movePage( $src_id, $dest_id, $location = 'inside' ) {
+
+ 		$src_xpath = sprintf( self::LEAF_XPATH, $this->leaf_id_prefix . $src_id );
+ 		$dest_xpath = sprintf( self::LEAF_XPATH, $this->leaf_id_prefix . $dest_id );
+ 		
+ 		// Find source and destination pages
+		$src = $this->webdriver->findElementBy( LocatorStrategy::xpath, $src_xpath );
+		// $dest = $this->webdriver->findElementBy( LocatorStrategy::id, $this->leaf_id_prefix . $dest_id );
+		$dest = $this->webdriver->findElementBy( LocatorStrategy::xpath, $dest_xpath );
+
+		// Store size for use calculating offsets
+		$dest_size = $dest->getSize();
+		// var_dump('Destination size: ' . print_r($dest_size,true) );
+
+		// Calculate offset based on desired movement location
+		$y_offset = 0;
+		$x_offset = intval($dest_size->width) / 2;
+
+		switch( $location ) {
+			case 'before':
+				// Relative to top-left of destination element
+				$y_offset = self::MOVE_BEFORE_Y_OFFSET;
+				break;
+
+			case 'after':
+				// Relative to top-left of destination element
+				$y_offset = intval($dest_size->height) + self::MOVE_AFTER_Y_OFFSET;
+				break;
+
+			case 'inside': default:
+				$y_offset = intval($dest_size->height) / 2;
+		}
+
+		// Select source page
+		$src->click();
+
+		// var_dump('Move location: ' . $location );
+		// var_dump('X offset: ' . $x_offset );
+		// var_dump('Y offset: ' . $y_offset );
+
+		// $file_before = "selenium/screenshots/{$src_id}_{$location}_{$dest_id}-1.png";
+		// $file_after = "selenium/screenshots/{$src_id}_{$location}_{$dest_id}-2.png";
+
+		// moveTo, buttownDown and buttonUp are not implemented in php-webdriver
+		// we have added them in to ours
+		$this->webdriver->moveTo( $src );
+		$this->webdriver->buttonDown();
+		$this->webdriver->moveTo( $dest, $x_offset, $y_offset );
+		// $this->webdriver->getScreenshotAndSaveToFile( $file_before );
+		$this->webdriver->buttonUp();
+		// $this->webdriver->getScreenshotAndSaveToFile( $file_after );
+
+	}
+
+	public function expandAll() {
+
+		$btn = $this->webdriver->findElementBy( LocatorStrategy::id, self::NAVMAN_EXPAND_BTN );
+		$btn->click();
+		sleep(1);
+
+	}
+
+	public function collapseAll() {
+
+		$btn = $this->webdriver->findElementBy( LocatorStrategy::id, self::NAVMAN_COLLAPSE_BTN );
+		$btn->click();
+		sleep(1);
+	}
+
+	public function save() {
+
+		$button = $this->webdriver->findElementBy( LocatorStrategy::id, self::NAVMAN_SAVE_BTN );
+		$button->click();
+		sleep(1);
+
+		$this->webdriver->getScreenshotAndSaveToFile( 'selenium/screenshots/after-save.png' );
+
+	}
+
+	/* Assertions */
+
+	const NAVMAN_SAVE_NOTIFICATION_XPATH = '//div[@id="message"]/p';
+
+	public function assertChangesWereSaved() {
+
+		$msg = $this->webdriver->findElementBy( LocatorStrategy::xpath, self::NAVMAN_SAVE_NOTIFICATION_XPATH );
+		$this->webdriver->assertEquals( $msg->getText(), 'Your navigation changes were saved.' );
+
+	}
+
+}
---
+++ tests-selenium/test_navman.php
@@ -0,0 +1,462 @@
+<?php
+
+require_once 'page-objects/navman.php';
+
+/**
+ * @group bu-navigation
+ * @group bu
+ */
+ class BU_Navigation_Navman_Test extends WP_SeleniumTestCase {
+
+	public function setUp() {
+
+		parent::setUp();
+
+		// Generate test pages and store ID's for test cases
+		$pid_one = $this->factory->post->create(array('post_title' => 'Parent page', 'post_type' => 'page' ) );
+		$pid_two = $this->factory->post->create(array('post_title' => 'Child page', 'post_parent' => $pid_one, 'post_type' => 'page' ) );
+		$pid_three = $this->factory->post->create(array('post_title' => 'Grand child page 1', 'post_parent' => $pid_two, 'post_type' => 'page' ) );
+		$pid_four = $this->factory->post->create(array('post_title' => 'Grand child page 2', 'post_parent' => $pid_two, 'post_type' => 'page' ) );
+		$pid_five = $this->factory->post->create(array('post_title' => 'Hidden page', 'post_type' => 'page' ) );
+		$pid_six = $this->factory->post->create(array('post_title' => 'Edit and Delete me', 'post_type' => 'page' ) );
+		$pid_seven = $this->factory->post->create(array('post_title' => 'Last page', 'post_type' => 'page' ) );
+
+		$this->pages = array(
+			'parent' => $pid_one,
+			'child' => $pid_two,
+			'grandchild_one' => $pid_three,
+			'grandchild_two' => $pid_four,
+			'hidden' => $pid_five,
+			'edit' => $pid_six,
+			'last_page' => $pid_seven
+			);
+
+		// Hide last page
+		update_post_meta( $pid_five, '_bu_cms_navigation_exclude', "1" );
+
+	}
+
+	public function tearDown() {
+
+		parent::tearDown();
+
+		foreach( $this->pages as $slug => $id ) {
+			wp_delete_post( $id, true );
+		}
+
+	}
+
+	/* Test types */
+
+	/**
+	 * @group bu-navigation-types
+	 */ 
+	public function test_section() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$page = $navman->getPage( $this->pages['parent'] );
+
+		$this->assertEquals( $page->getAttribute('rel'), BUN_Navman_Page::TYPES_FOLDER );
+
+	}
+
+	/**
+	 * @group bu-navigation-types
+	 */ 
+	public function test_leaf() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$page = $navman->getPage( $this->pages['last_page'] );
+		
+		$this->assertEquals( $page->getAttribute('rel'), BUN_Navman_Page::TYPES_PAGE );
+
+	}
+
+	/**
+	 * @group bu-navigation-types
+	 */ 
+	public function test_hidden_page() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$page = $navman->getPage( $this->pages['hidden'] );
+
+		$this->assertEquals( $page->getAttribute('rel'), BUN_Navman_Page::TYPES_EXCLUDED );
+
+	}
+
+	/**
+	 * ACL - Restricted
+	 */ 
+	// public function test_restricted_page() {
+
+	// }
+
+	/**
+	 * BU Section Editing - Denied
+	 */
+	// public function test_denied_page() {
+
+	// } 
+
+	/* Basic tree interactions */
+
+	/**
+	 * @group bu-navigation-actions
+	 * 
+	 * @todo utilize page object for class attributes
+	 */ 
+	public function test_hover() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$page = $navman->getPage( $this->pages['parent'], 'a' );
+
+		$this->moveTo( $page );
+
+		$this->assertTrue( false !== strpos( $page->getAttribute('class'), 'jstree-hovered' ) );
+
+	}
+
+	/**
+	 * @group bu-navigation-actions
+	 * @todo utilize page object for class attributes
+	 */ 
+	public function test_select() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$page = $navman->getPage( $this->pages['parent'], 'a' );
+
+		$page->click();
+
+		$this->assertTrue( false !== strpos( $page->getAttribute('class'), 'jstree-clicked' ) );
+
+	}
+
+	/**
+	 * @group bu-navigation-actions
+	 * 
+	 * @todo utilize page object for class attributes
+	 */ 
+	public function test_open() {
+		
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$page = $navman->getPage( $this->pages['parent'] );
+
+		$this->assertTrue( false !== strpos( $page->getAttribute('class'), 'jstree-closed' ) );
+
+		$navman->openSection( $this->pages['parent'] );
+
+		$this->assertTrue( false !== strpos( $page->getAttribute('class'), 'jstree-open' ) );
+
+	}
+
+
+	/**
+	 * @group bu-navigation-actions
+	 * 
+	 * @todo utilize page object for class attributes
+	 */ 
+	public function test_expand_all(){
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		$sections = $navman->getSections();
+
+		foreach( $sections as $section ) {
+			$this->assertRegExp( '/jstree-closed/', $section->getAttribute('class') );
+		}
+
+		$navman->expandAll();
+
+		foreach( $sections as $section ) {
+			$this->assertRegExp( '/jstree-open/', $section->getAttribute('class') );
+		}
+
+	}
+
+	/**
+	 * @group bu-navigation-actions
+	 * 
+	 * @todo utilize page object for class attributes
+	 */ 
+	public function test_collapse_all() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$navman->expandAll();
+
+		$sections = $navman->getSections();
+
+		foreach( $sections as $section ) {
+			$this->assertRegExp( '/jstree-open/', $section->getAttribute('class') );
+		}
+
+		$navman->collapseAll();
+
+		foreach( $sections as $section ) {
+			$this->assertRegExp( '/jstree-closed/', $section->getAttribute('class') );
+		}
+
+	}
+
+	/* Page movement */
+
+	/**
+	 * @group bu-navigation-moves
+	 */ 
+	public function test_move_page_before() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$src_id = $this->pages['grandchild_two'];
+		$dest_id = $this->pages['grandchild_one'];
+
+		// Open parent sections and move page
+		$navman->openSection( $this->pages['parent'] );
+		$navman->openSection( $this->pages['child'] );
+		$navman->movePage( $src_id, $dest_id, 'before' );
+
+		// Verify move
+		$selector = sprintf("#p%s + #p%s", $src_id, $dest_id );
+		$newPage = $this->findElementBy( LocatorStrategy::cssSelector, $selector );
+		$this->assertNotNull( $newPage );
+
+	}
+
+	/**
+	 * @group bu-navigation-moves
+	 */ 
+	public function test_move_page_after() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$src_id = $this->pages['grandchild_one'];
+		$dest_id = $this->pages['grandchild_two'];
+
+		// Open parent sections and move page
+		$navman->openSection( $this->pages['parent'] );
+		$navman->openSection( $this->pages['child'] );
+		$navman->movePage( $src_id, $dest_id, 'after' );
+
+		// Verify move
+		$selector = sprintf("#p%s + #p%s", $dest_id, $src_id );
+		$newPage = $this->findElementBy( LocatorStrategy::cssSelector, $selector );
+		$this->assertNotNull( $newPage );
+
+	}
+
+	/**
+	 * @group bu-navigation-moves
+	 */ 
+	public function test_move_page_inside() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+		$src_id = $this->pages['grandchild_two'];
+		$dest_id = $this->pages['grandchild_one'];
+
+		// Open parent sections and move page
+		$navman->openSection( $this->pages['parent'] );
+		$navman->openSection( $this->pages['child'] );
+		$navman->movePage( $src_id, $dest_id, 'inside' );
+
+		// Verify move
+		$selector = sprintf("#p%s > ul > #p%s", $dest_id, $src_id );
+		$newPage = $this->findElementBy( LocatorStrategy::cssSelector, $selector );
+		$this->assertNotNull( $newPage );
+
+	}
+
+	/**
+	 * @group bu-navigation-actions
+	 */
+	public function test_edit_page() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		// Select and edit a page
+		$navman->editPage( $this->pages['edit'] );
+
+		// Check new URL
+		$url = $this->getCurrentUrl();
+		$edit_id = $this->pages['edit'];
+		$this->assertRegExp( "/wp\-admin\/post\.php\?action=edit&post=$edit_id/", $this->getCurrentUrl() );
+
+	}
+
+	/**
+	 * @group bu-navigation-actions 
+	 * @expectedException NoSuchElementException
+	 */
+	public function test_delete_page() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		// Select and remove a page
+		$navman->deletePage( $this->pages['edit'] );
+
+		// Will throw an expected NoSuchElementException
+		$page = $navman->getPage( $this->pages['edit'] );
+
+	}
+
+	/**
+	 * @group bu-navigation-actions
+	 */
+	public function test_dirty_alert_leave() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		// Delete a page and then attempt to edit one, which prompts the diry warning
+		$navman->deletePage( $this->pages['edit'] );
+		$navman->editPage( $this->pages['last_page'] );
+
+		// Confirm presence of alert
+		$txt = $this->getAlertText();
+		$this->assertEquals( $txt, 'This page is asking you to confirm that you want to leave - data you have entered may not be saved.');
+
+		// Accept alert (leave this page)
+		$this->acceptAlert();
+
+		sleep(1);
+
+		// Confirm new URL
+		$url = $this->getCurrentUrl();
+		$edit_id = $this->pages['last_page'];
+		$this->assertRegExp( "/wp\-admin\/post\.php\?action=edit&post=$edit_id/", $this->getCurrentUrl() );
+
+	}
+
+	/**
+	 * @group bu-navigation-actions
+	 */
+	public function test_dirty_alert_stay() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		$url_before = $this->getCurrentUrl();
+
+		// Delete a page and then attempt to edit one, which prompts the diry warning
+		$navman->deletePage( $this->pages['edit'] );
+		$navman->editPage( $this->pages['last_page'] );
+
+		// Confirm presence of alert
+		$txt = $this->getAlertText();
+		$this->assertEquals( $txt, 'This page is asking you to confirm that you want to leave - data you have entered may not be saved.');
+
+		// Dismiss alert (stay on this page)
+		$this->dismissAlert();
+		sleep(1);
+
+		// Confirm URL is the same
+		$url_after = $this->getCurrentUrl();
+		$this->assertEquals( $url_before, $url_after );
+
+	}
+
+	/**
+	 * @group bu-navigation-save
+	 */
+	public function test_save_changes() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		// Save and verify changes were committed
+		$navman->save();
+		$navman->assertChangesWereSaved();
+
+	}
+
+	/**
+	 * @group bu-navigation-save
+	 */
+	public function test_save_with_moves() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		$src_id = $this->pages['grandchild_two'];
+		$dest_id = $this->pages['grandchild_one'];
+
+		// Open parent sections
+		$navman->openSection( $this->pages['parent'] );
+		$navman->openSection( $this->pages['child'] );
+		$navman->movePage( $src_id, $dest_id, 'inside' );
+
+		// Save and verify changes were committed
+		$navman->save();
+		$navman->assertChangesWereSaved();
+
+		// Reopen sections
+		$navman->openSection( $this->pages['parent'] );
+		$navman->openSection( $this->pages['child'] );
+		$navman->openSection( $this->pages['grandchild_one'] );
+
+		// Verify move
+		$selector = sprintf("#p%s > ul > #p%s", $dest_id, $src_id );
+		$newPage = $this->findElementBy( LocatorStrategy::cssSelector, $selector );
+		$this->assertNotNull( $newPage );
+
+	}
+
+	/**
+	 * @group bu-navigation-save
+	 * @expectedException NoSuchElementException
+	 */
+	public function test_save_with_deletions() {
+
+		$navman = new BUN_Navman_Page( $this, 'page' );
+
+		// Remove page
+		$navman->deletePage( $this->pages['parent'] );
+
+		// Save and verify changes were committed
+		$navman->save();
+		$navman->assertChangesWereSaved();
+
+		// Will throw an expected NoSuchElementException
+		$navman->getPage( $this->pages['parent'] );
+
+	}
+
+	/* @todo Links */
+
+	// public function test_add_link() {
+
+	// }
+
+	// public function test_edit_link() {
+
+	// }
+
+	// public function test_delete_link() {
+
+	// }
+
+	/**
+	 * Contextual menu 
+	 * 
+	 * @todo right clicking is not implemented in webdriver at the moment
+	 */
+
+	// public function test_context_open() {
+
+	// 	$navman = new BUN_Navman_Page( $this, 'page' );
+	// 	$page = $navman->getPage( $this->pages['parent'], 'a' );
+
+	// 	$menu = $this->getElement( LocatorStrategy::id, 'vakata-contextmenu' );
+
+	// 	$this->assertEquals( $menu->getCssProperty('visibility'), 'hidden' ));
+
+  // 		// Move the mouse and right click to open
+	// 	$this->moveTo( $page, 5, 5 );
+	// 	$this->buttonDown( 2 ); // 2 = right click
+	// 	$this->buttonUp( 2 ); // 2 = right click
+
+	// 	$this->assertEquals( $menu->getCssProperty('visibility'), 'visible' ));
+
+	// }
+
+	// public function test_context_edit() {
+
+	// }
+
+	// public function test_context_remove() {
+
+	// }
+
+ }

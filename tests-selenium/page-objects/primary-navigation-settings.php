638b2a978908c01a51ca7de9f9c699b8f59835af
mgburns <mgburns@00b09ed4-20f5-4458-a511-c3642c8cf453>
2012-10-01 20:17:18 +0000

    Selenium tests for primary navigation settings page

--- tests-selenium/page-objects/primary-navigation-settings.php
+++ tests-selenium/page-objects/primary-navigation-settings.php
@@ -6,8 +6,22 @@
 class BUN_Settings_Page {
 
 	/* URL's */
-	const URL = '/wp-admin/edit.php?page=bu-navigation/bu-navman.php';
-	const PAGE_HEADING_XPATH = "//h2[contains(text(),'Edit Navigation')]";
+	const URL = '/wp-admin/themes.php?page=bu-navigation-settings';
+	const PAGE_HEADING_XPATH = "//h2[contains(text(),'Primary Navigation')]";
+
+	const FORM_ID = 'bu_navigation_primary_navigation';
+	private $form;
+	private $option_fields = array(
+		'display' => array( 'name' => 'bu_navigation_primarynav', 'type' => 'checkbox' ),
+		'max_items' => array( 'name' => 'bu_navigation_primarynav_max', 'type' => 'text' ),
+		'dive' => array( 'name' => 'bu_navigation_primarynav_dive', 'type' => 'checkbox' ),
+		'depth' => array( 'name' => 'bu_navigation_primarynav_depth', 'type' => 'select' ),
+		'allow_top' => array( 'name' => 'bu_allow_top_level_page', 'type' => 'checkbox' ),
+		);
+
+	// Xpath templates
+	const NOTICE_UPDATED_XPATH = '//div[contains(@class,"updated")]';
+	const NOTICE_ERRORS_XPATH = '//div[contains(@class,"error")]';
 
 	/**
 	 * Load the navigation settings page
@@ -24,6 +38,52 @@ class BUN_Settings_Page {
 		} catch( NoSuchElementException $e ) {
 			throw new Exception('BU Navigation Settings page failed to load with URL: ' . self::URL );
 		}
+
+		$this->form = new SeleniumFormHelper( $webdriver, self::FORM_ID );
+
+	}
+
+	public function setOptions( $fields ) {
+
+		$formdata = array();
+
+		foreach( $fields as $key => $val ) {
+			$field = $this->option_fields[$key];
+
+			// Format data for form helper
+			$formdata[$field['name']] = array( 'type' => $field['type'], 'value' => $val );
+		}
+
+		if( ! empty( $formdata ) )
+			$this->form->populateFields( $formdata );
+
+	}
+
+	public function getOption( $key ) {
+
+		$field = $this->option_fields[$key];
+
+		return $this->form->getFieldValue( $field['name'] );
+
+	}
+
+	public function save() {
+
+		$this->form->submit();
+
+	}
+
+	/* Assertions */
+
+	public function assertChangesWereSaved() {
+
+		$notice = $this->webdriver->getElement( LocatorStrategy::xpath, self::NOTICE_UPDATED_XPATH );
+
+	}
+
+	public function assertErrorSavingChanges() {
+
+		$notice = $this->webdriver->getElement( LocatorStrategy::xpath, self::NOTICE_ERRORS_XPATH );
 
 	}
 

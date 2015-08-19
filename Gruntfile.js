431076e2a871e33fb1729cf27e25a95f918e68b1
Andrew Bauer <awbauer@bu.edu>
2015-08-19 12:36:57 -0400

    adding l18n stuff to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,39 @@ module.exports = function(grunt) {
 				files: ['js/**/*.js', '!js/**/*.min.js'],
 				tasks: ['uglify']
 			}
-		}
+		},
+		po2mo: {
+		    files: {
+		        src: 'languages/*.po',
+		        expand: true,
+		    },
+		},
+		pot: {
+		      options:{
+		          text_domain: 'bu-navigation',
+		          dest: 'languages/',
+		          keywords: [ // WordPress localisation functions
+		            '__:1',
+		            '_e:1',
+		            '_x:1,2c',
+		            'esc_html__:1',
+		            'esc_html_e:1',
+		            'esc_html_x:1,2c',
+		            'esc_attr__:1', 
+		            'esc_attr_e:1', 
+		            'esc_attr_x:1,2c', 
+		            '_ex:1,2c',
+		            '_n:1,2', 
+		            '_nx:1,2,4c',
+		            '_n_noop:1,2',
+		            '_nx_noop:1,2,3c'
+		           ],
+		      },
+		      files:{
+		          src:  [ '**/*.php' ],
+		          expand: true,
+		      }
+		},
 	});
 
 	// Build task.

8dfba6778052277ec63ad8fb11dfa414ec75a5b0
Jon Desrosiers <desrosjaccounts@gmail.com>
2018-02-12 10:24:31 -0500

    Add support for Post Type Templates in WordPress 4.7
    See: https://make.wordpress.org/core/2016/11/03/post-type-templates-in-4-7/

--- admin/post.php
+++ admin/post.php
@@ -128,7 +128,7 @@ class BU_Navigation_Admin_Post {
 		// Remove built in page attributes meta box
 		remove_meta_box('pageparentdiv', $post_type, 'side');
 
-		$templates = get_page_templates();
+		$templates = get_page_templates( $post_type );
 
 		if ( 'page' === $post_type && is_array( $templates ) && ( count( $templates ) > 0 ) ) {
 			add_meta_box(
@@ -198,6 +198,8 @@ class BU_Navigation_Admin_Post {
 	 * Since we replace the standard "Page Attributes" meta box with our own,
 	 * we relocate the "Template" dropdown that usually appears there to its
 	 * own custom meta box
+	 *
+	 * @param WP_Post $post Current post object.
 	 */
 	public function display_custom_template( $post ) {
 

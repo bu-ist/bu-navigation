05c2f1bffd239bb79f83c7ba9cca35071e86e4ee
Anton Kachurin <kachurinanton@gmail.com>
2018-03-23 10:22:17 -0400

    Use post type name instead of post type object

--- templates/metabox-custom-template.php
+++ templates/metabox-custom-template.php
@@ -2,6 +2,6 @@
 <label class="screen-reader-text" for="page_template"><?php printf( __( '%s Template', 'bu-navigation' ), $post_type->labels->singular_name ); ?></label>
 <select name="page_template" id="page_template">
 	<option value='default'><?php _e( 'Default Template', 'bu-navigation' ); ?></option>
-	<?php page_template_dropdown( $current_template, $post_type ); ?>
+	<?php page_template_dropdown( $current_template, $post_type->name ); ?>
 </select>
 <p><?php printf( __( 'Some themes have custom templates you can use for certain %s that might have additional features or custom layouts.', 'bu-navigation' ), strtolower( $post_type->labels->name ) ); ?></p>

8236fdcb74a4cfeaf7449a00ff552d67b411d883
Jonathan Williams <tamlin@mac.com>
2020-09-02 14:12:07 -0400

    :art: fix package and license docblocks

--- bu-navigation.php
+++ bu-navigation.php
@@ -1,31 +1,35 @@
 <?php
-/*
-Plugin Name: BU Navigation
-Plugin URI: http://developer.bu.edu/bu-navigation/
-Author: Boston University (IS&T)
-Author URI: http://sites.bu.edu/web/
-Description: Provides alternative navigation elements designed for blogs with large page counts
-Version: 1.2.20
-Text Domain: bu-navigation
-Domain Path: /languages
-*/
+/**
+ * Plugin Name: BU Navigation
+ * Plugin URI: http://developer.bu.edu/bu-navigation/
+ * Author: Boston University (IS&T)
+ * Author URI: http://sites.bu.edu/web/
+ * Description: Provides alternative navigation elements designed for blogs with large page counts
+ * Version: 1.2.20
+ * Text Domain: bu-navigation
+ * Domain Path: /languages
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package BU_Navigation
+ */
 
 /**
-Copyright 2014 by Boston University
+ * Copyright 2014 by Boston University
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  **/
 
 /*

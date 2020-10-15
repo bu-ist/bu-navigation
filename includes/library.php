f4971cf53c585145d89f2da98b55e0b8483828c4
Jonathan Williams <tamlin@mac.com>
2020-10-15 17:27:47 -0400

    :art: refactor load_sections results transform
    by breaking the transform code out into a separate function.  Also unwraps the multilevel nest of iterators and conditionals to some extent.  Complexity scores from cyclomatic to NPath drop significantly.

--- includes/library.php
+++ includes/library.php
@@ -125,29 +125,51 @@ function bu_navigation_load_sections( $post_types = array(), $include_links = tr
 	);
 	$rows  = $wpdb->get_results( $query );
 
-	$sections = array();
-	$pages    = array();
-
-	if ( is_array( $rows ) && ( count( $rows ) > 0 ) ) {
-		foreach ( $rows as $row ) {
-			$sections[ $row->section ] = explode( ',', $row->children );
-
-			if ( is_array( $sections[ $row->section ] ) && ( count( $sections[ $row->section ] ) > 0 ) ) {
-				foreach ( $sections[ $row->section ] as $child ) {
-					$pages[ $child ] = $row->section;
-				}
-			}
-		}
-	}
+	$all_sections = bu_navigation_transform_rows( $rows );
 
 	// Cache results.
-	$all_sections = array(
-		'sections' => $sections,
-		'pages'    => $pages,
-	);
 	wp_cache_set( $cache_key, $all_sections, 'bu-navigation' );
 
 	return $all_sections;
+}
+
+/**
+ * Takes the results of the custom parents query and maps them into the 'section' and 'pages' format.
+ *
+ * @since 1.2.24
+ *
+ * @param array $rows Array of objects from $wpdb, where each object has a 'section' and 'children property.
+ * @return array
+ */
+function bu_navigation_transform_rows( $rows ) {
+
+	// If $rows is malformed or empty, return an empty result.
+	if ( ! is_array( $rows ) || 0 === count( $rows ) ) {
+		return array(
+			'sections' => array(),
+			'pages'    => array(),
+		);
+	}
+
+	// Construct the 'section' array with elements where the key is the parent post ID and the value is an array of child post ids.
+	$sections = array();
+	foreach ( $rows as $row ) {
+		$sections[ $row->section ] = explode( ',', $row->children );
+	}
+
+	// Construct the 'pages' array with elements where the key is the child id and the value is the parent id.
+	// Seems like something like array_reduce() would be more elegant, but returning significant keys is a challenge.
+	$pages = array();
+	foreach ( $sections as $parent_id => $children_ids ) {
+		foreach ( $children_ids as $child_id ) {
+			$pages[ $child_id ] = strval( $parent_id );
+		}
+	}
+
+	return array(
+		'sections' => $sections,
+		'pages'    => $pages,
+	);
 }
 
 /**

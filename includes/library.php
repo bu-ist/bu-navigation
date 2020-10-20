71ead5c9557849b67064d39a18529c5aae004ed4
Jonathan Williams <tamlin@mac.com>
2020-10-20 17:24:41 -0400

    :recycle: refactor adaptive section handling
    from list_pages to a separate function.

--- includes/library.php
+++ includes/library.php
@@ -1026,6 +1026,10 @@ function bu_navigation_list_pages( $args = '' ) {
 
 	$sections = ! empty( $parsed_args['sections'] ) ? $parsed_args['sections'] : array_keys( $pages_by_parent );
 
+	if ( 'adaptive' === $parsed_args['style'] ) {
+		$sections = adaptive_section_slice( $parsed_args['page_id'], $pages_by_parent, $sections );
+	}
+
 	$list_attributes = '';
 
 	if ( $parsed_args['container_id'] ) {
@@ -1037,30 +1041,6 @@ function bu_navigation_list_pages( $args = '' ) {
 
 	$html = sprintf( "<%s %s>\n", $parsed_args['container_tag'], $list_attributes );
 
-	if ( $parsed_args['style'] == 'adaptive' ) {
-
-		// If the "active" page isn't in the list of sections (because it has no children), add it
-		// @todo I don't think this can ever be true based on the code in bu-navigation-adaptive-contentnav.php
-		if ( $parsed_args['page_id'] && ! in_array( $parsed_args['page_id'], $sections ) ) {
-			array_push( $sections, $parsed_args['page_id'] );
-		}
-
-		if ( count( $sections ) > 2 ) {
-			$last_section = array_pop( $sections );
-			array_push( $sections, $last_section );
-
-			if ( array_key_exists( $last_section, $pages_by_parent )
-				&& is_array( $pages_by_parent[ $last_section ] )
-				&& ( count( $pages_by_parent[ $last_section ] ) > 0 )
-			) {
-				// Last section has children, so it will be the "top"
-				$sections = array_slice( $sections, -2 );
-			} else {
-				// Last section has no children, so its parent will be the "top"
-				$sections = array_slice( $sections, -3 );
-			}
-		}
-	}
 
 	// Default to top level pages.
 	$section = $sections[0];
@@ -1115,6 +1095,46 @@ function bu_navigation_list_pages( $args = '' ) {
 	}
 
 	return $html;
+}
+
+/**
+ * Return a slice of the section elements for the 'adaptive' widget mode
+ *
+ * The page_id parameter should be examined in conjunction with the widget_bu_pages_args filter,
+ * which may be an artifact than can be unwound.
+ *
+ * @since 1.2.24
+ * @see bu_navigation_list_pages()
+ *
+ * @param mixed $page_id String or int; see note below, this is likely never anything but null due to the widget_bu_pages_args filter.
+ * @param array $pages_by_parent Array where the key is a post id, and the value is an array of post objects.
+ * @param array $sections Array of post IDs, either strings or ints.
+ * @return array Array of post ids.
+ */
+function adaptive_section_slice( $page_id, $pages_by_parent, $sections ) {
+	// If the "active" page isn't in the list of sections (because it has no children), add it
+	// @todo I don't think this can ever be true based on the code in bu-navigation-adaptive-contentnav.php
+	if ( $page_id && ! in_array( $page_id, $sections, true ) ) {
+		array_push( $sections, $page_id );
+	}
+
+	if ( count( $sections ) > 2 ) {
+		$last_section = array_pop( $sections );
+		array_push( $sections, $last_section );
+
+		if ( array_key_exists( $last_section, $pages_by_parent )
+			&& is_array( $pages_by_parent[ $last_section ] )
+			&& ( count( $pages_by_parent[ $last_section ] ) > 0 )
+		) {
+			// Last section has children, so it will be the "top"
+			$sections = array_slice( $sections, -2 );
+		} else {
+			// Last section has no children, so its parent will be the "top"
+			$sections = array_slice( $sections, -3 );
+		}
+	}
+
+	return $sections;
 }
 
 /**

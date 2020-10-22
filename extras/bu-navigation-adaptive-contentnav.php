dd0ad7fe7f1609227c404e76cdd62a6974cc502a
Jonathan Williams <tamlin@mac.com>
2020-10-22 17:30:58 -0400

    :memo: add parameter comments
    to docblocks

--- extras/bu-navigation-adaptive-contentnav.php
+++ extras/bu-navigation-adaptive-contentnav.php
@@ -4,6 +4,8 @@
  * "drilling down" based on the currently active post, limiting  listings
  * to no more than two levels (plus section title if configured).
  *
+ * @package BU_Navigation
+ *
  * @see [Content Navigation Widget Modes](https://github.com/bu-ist/bu-navigation/wiki/Content-Navigation-Widget)
  * @todo bu_navigation_list_pages has a lot of adaptive mode logic -- refactor to consolidate
  */
@@ -13,6 +15,9 @@
  *
  * This is an ugly way of short circuiting the logic within bu_navigation_list_pages to not
  * display all sections.
+ *
+ * @param array $args Associative array of arguments for the list pages query.
+ * @return array Array of arguments transformed for adaptive mode.
  */
 function widget_bu_pages_args_adaptive( $args ) {
 	if ( $args['page_id'] ) {
@@ -28,6 +33,9 @@ function widget_bu_pages_args_adaptive( $args ) {
 /**
  * Filters posts returned from bu_navigation_pages_by_parent to only include those
  * centered around the current post
+ *
+ * @param array $pages_by_parent Array of parents and children in the form of a 'section'.
+ * @return array Transformed array for adaptive mode.
  */
 function bu_navigation_filter_pages_adaptive( $pages_by_parent ) {
 	global $post;

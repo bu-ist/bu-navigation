5f5a42d3b398b0d1f047ec239ea3ceac774d31ae
Jonathan Williams <tamlin@mac.com>
2020-11-25 11:31:33 -0500

    :recycle: move get_page_uri
    to data-get-urls

--- includes/data-get-urls.php
+++ includes/data-get-urls.php
@@ -8,6 +8,67 @@
 namespace BU\Plugins\Navigation;
 
 /**
+ * Calculate the post path for a post.
+ *
+ * Loops backwards from $page through $ancestors to determine full post path.
+ * If any ancestor is not present in $ancestors it will attempt to load them on demand.
+ * Utilizes static caching to minimize repeat queries across calls.
+ *
+ * @param  object $page      Post object to query path for. Must contain ID, post_name and post_parent fields.
+ * @param  array  $ancestors An array of post objects keyed on post ID.  Should contain ancestors of $page,
+ *                           with ID, post_name and post_parent fields for each.
+ * @return string            Page path.
+ */
+function get_page_uri( $page, $ancestors ) {
+
+	// Used to cache pages we load that aren't contained in $ancestors.
+	static $extra_pages   = array();
+	static $missing_pages = array();
+
+	$uri = $page->post_name;
+
+	while ( isset( $page->post_parent ) && $page->post_parent != 0 ) {
+
+		// Avoid infinite loops
+		if ( $page->post_parent == $page->ID ) {
+			break;
+		}
+
+		// Attempt to load missing ancestors.
+		if ( ! array_key_exists( $page->post_parent, $ancestors ) ) {
+			if ( ! array_key_exists( $page->post_parent, $extra_pages ) && ! in_array( $page->post_parent, $missing_pages ) ) {
+				$missing_ancestors = get_page_uri_ancestors( $page );
+				// Cache any ancestors we load here or can't find in separate data structures.
+				if ( ! empty( $missing_ancestors ) ) {
+					$extra_pages = $extra_pages + $missing_ancestors;
+				} else {
+					// Add to our tracking list of pages we've already looked for.
+					$missing_pages[] = $page->post_parent;
+				}
+			}
+
+			// Merge passed in ancestors with extras we've loaded along the way.
+			$ancestors = $ancestors + $extra_pages;
+		}
+
+		// We can't return an incomplete path -- bail with indication of failure.
+		if ( ! array_key_exists( $page->post_parent, $ancestors ) ) {
+			break;
+		}
+
+		// Append parent post name and keep looping backwards.
+		$parent = $ancestors[ $page->post_parent ];
+		if ( is_object( $parent ) && ! empty( $parent->post_name ) ) {
+			$uri = $parent->post_name . '/' . $uri;
+		}
+
+		$page = $parent;
+	}
+
+	return $uri;
+}
+
+/**
  * Undocumented function
  *
  * Docs in progress.
--- includes/library.php
+++ includes/library.php
@@ -313,58 +313,16 @@ function bu_navigation_get_post_link( $post, $ancestors = array(), $sample = fal
  * If any ancestor is not present in $ancestors it will attempt to load them on demand.
  * Utilizes static caching to minimize repeat queries across calls.
  *
+ * This is a stub for the new namespaced function, but there's no reason to think
+ * any other themes or components are calling this. Likely it should be removed.
+ *
  * @param  object $page      Post object to query path for. Must contain ID, post_name and post_parent fields.
  * @param  array  $ancestors An array of post objects keyed on post ID.  Should contain ancestors of $page,
  *                           with ID, post_name and post_parent fields for each.
  * @return string            Page path.
  */
 function bu_navigation_get_page_uri( $page, $ancestors ) {
-
-	// Used to cache pages we load that aren't contained in $ancestors.
-	static $extra_pages   = array();
-	static $missing_pages = array();
-
-	$uri = $page->post_name;
-
-	while ( isset( $page->post_parent ) && $page->post_parent != 0 ) {
-
-		// Avoid infinite loops
-		if ( $page->post_parent == $page->ID ) {
-			break;
-		}
-
-		// Attempt to load missing ancestors.
-		if ( ! array_key_exists( $page->post_parent, $ancestors ) ) {
-			if ( ! array_key_exists( $page->post_parent, $extra_pages ) && ! in_array( $page->post_parent, $missing_pages ) ) {
-				$missing_ancestors = Navigation\get_page_uri_ancestors( $page );
-				// Cache any ancestors we load here or can't find in separate data structures.
-				if ( ! empty( $missing_ancestors ) ) {
-					$extra_pages = $extra_pages + $missing_ancestors;
-				} else {
-					// Add to our tracking list of pages we've already looked for.
-					$missing_pages[] = $page->post_parent;
-				}
-			}
-
-			// Merge passed in ancestors with extras we've loaded along the way.
-			$ancestors = $ancestors + $extra_pages;
-		}
-
-		// We can't return an incomplete path -- bail with indication of failure.
-		if ( ! array_key_exists( $page->post_parent, $ancestors ) ) {
-			break;
-		}
-
-		// Append parent post name and keep looping backwards.
-		$parent = $ancestors[ $page->post_parent ];
-		if ( is_object( $parent ) && ! empty( $parent->post_name ) ) {
-			$uri = $parent->post_name . '/' . $uri;
-		}
-
-		$page = $parent;
-	}
-
-	return $uri;
+	return Navigation\get_page_uri( $page, $ancestors );
 }
 
 /**

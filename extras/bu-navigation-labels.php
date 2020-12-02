7155908803855862f93da46b52e3985d73bfea03
Jonathan Williams <tamlin@mac.com>
2020-12-02 15:57:19 -0500

    :art: spacing fixes and comments

--- extras/bu-navigation-labels.php
+++ extras/bu-navigation-labels.php
@@ -1,12 +1,24 @@
 <?php
-// Name of meta_key used to hold navigation labels
-define('BU_NAV_META_PAGE_LABEL', '_bu_cms_navigation_page_label');
+/**
+ * Navigation post_meta labels
+ *
+ * Functions that load and attach the navigation label to posts.
+ *
+ * @package BU_Navigation
+ */
+
+// Name of meta_key used to hold navigation labels.
+define( 'BU_NAV_META_PAGE_LABEL', '_bu_cms_navigation_page_label' );
 
 /**
  * Built-in filter for bu_navigation_get_pages
  *
  * Adds a "navigation_label" property to each post object, fed from
- * the "Placement in Navigation" "metabox "Label" text field
+ * the "Placement in Navigation" "metabox "Label" text field.
+ *
+ * @param array $pages Array of objects representing individual posts.
+ *
+ * @return array Array of objects representing individual posts with the navigation_label added
  */
 function bu_navigation_filter_pages_navlabels( $pages ) {
 	global $wpdb;
@@ -15,18 +27,20 @@ function bu_navigation_filter_pages_navlabels( $pages ) {
 
 	if ( is_array( $pages ) && count( $pages ) > 0 ) {
 
-		$ids = array_keys( $pages );
-		$query = sprintf( "SELECT post_id, meta_value FROM %s WHERE meta_key = '%s' AND post_id IN (%s) AND meta_value != ''",
+		$ids   = array_keys( $pages );
+		$query = sprintf(
+			"SELECT post_id, meta_value FROM %s WHERE meta_key = '%s' AND post_id IN (%s) AND meta_value != ''",
 			$wpdb->postmeta,
 			BU_NAV_META_PAGE_LABEL,
 			implode( ',', $ids )
-			);
+		);
+
 		$labels = $wpdb->get_results( $query, OBJECT_K );
 
 		if ( is_array( $labels ) && count( $labels ) > 0 ) {
 			foreach ( $pages as $page ) {
 				if ( array_key_exists( $page->ID, $labels ) ) {
-					$label = $labels[ $page->ID ];
+					$label                  = $labels[ $page->ID ];
 					$page->navigation_label = $label->meta_value;
 				}
 				$filtered[ $page->ID ] = $page;
@@ -48,26 +62,26 @@ add_filter( 'bu_navigation_filter_page_labels', 'bu_navigation_filter_pages_navl
  * Content editors set this value through the "Label" text field in the
  * "Placement in Navigation" metabox.
  *
- * @param mixed $post post ID or object to fetch label for
- * @param string $empty_label label to use if no existing value was found
- * @return string the post's navigation, or $empty_label if none was found
+ * @param mixed  $post Post ID or object to fetch label for.
+ * @param string $empty_label Label to use if no existing value was found.
+ * @return string The post's navigation label, or $empty_label if none was found
  */
 function bu_navigation_get_label( $post, $empty_label = '(no title)' ) {
-	if( is_numeric( $post ) ) {
+	if ( is_numeric( $post ) ) {
 		$post = get_post( $post );
 	}
 
-	if( ! is_object( $post ) ) {
+	if ( ! is_object( $post ) ) {
 		return false;
 	}
 
 	$label = get_post_meta( $post->ID, BU_NAV_META_PAGE_LABEL, true );
 
-	if( ! $label ) {
+	if ( ! $label ) {
 		$label = $post->post_title;
 	}
 
-	if( empty( $label ) ) {
+	if ( empty( $label ) ) {
 		$label = $empty_label;
 	}
 
